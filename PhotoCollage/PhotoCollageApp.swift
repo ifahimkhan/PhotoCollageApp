09057f295fe7eb525b017653dcb1d299fbca781e
ifahimkhan <khan.fahim95@hotmail.com>
2024-03-19 16:44:03 +0400

    Initial Commit

---
+++ PhotoCollage/PhotoCollageApp.swift
@@ -0,0 +1,17 @@
+//
+//  PhotoCollageApp.swift
+//  PhotoCollage
+//
+//  Created by Ragesh on 3/19/24.
+//
+
+import SwiftUI
+
+@main
+struct PhotoCollageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
