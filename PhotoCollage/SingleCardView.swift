0303fed1887eea75ef94e66f2fa5f94961e0272f
ifahimkhan <khan.fahim95@hotmail.com>
2024-03-21 03:40:30 +0400

    if applied this commit will add toolbars navigation Stack and Navigation Bar

--- PhotoCollage/SingleCardView.swift
+++ PhotoCollage/SingleCardView.swift
@@ -8,8 +8,23 @@
 import SwiftUI
 
 struct SingleCardView: View {
+    @Environment(\.dismiss) var dismiss
+    @State private var currentModal: ToolbarSelection?
+
     var body: some View {
-        Color.yellow
+        NavigationStack {
+            Color.yellow
+                .toolbar{
+                    ToolbarItem(placement: .navigationBarTrailing){
+                        Button("Done") {
+                            dismiss()
+                        }
+                    }
+                    ToolbarItem(placement: .bottomBar){
+                        BottomToolbar(modal: $currentModal)
+                    }
+                }
+        }
     }
 }
 
---
+++ PhotoCollage/ToolbarSelection.swift
@@ -0,0 +1,12 @@
+//
+//  ToolbarSelection.swift
+//  PhotoCollage
+//
+//  Created by Fahim on 3/20/24.
+//
+
+import Foundation
+
+enum ToolbarSelection: CaseIterable{
+   case photoModal,frameModal,stickerModal,textModal
+}
